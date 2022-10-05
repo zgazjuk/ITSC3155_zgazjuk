c35fc3258209f6f717e1415fd06a8d68ef5bca90
zgazjuk <zgazjuk@icloud.com>
2022-10-04 22:27:24 -0400

    PythonBasics2 - Update method

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -9,32 +9,67 @@
 # Define a function count_threes(n) that takes an int and
 # returns the number of multiples of 3 in the range from 0
 # to n (including n).
-
+import math;
 def count_threes(n):
-  count = 0
-  for i in range(0, n + 1):
-    if i % 3 == 0:
-      count = count + 1
-  return count
+  multiple = {
+    "9": 0,
+    "6": 0,
+    "3": 0
+  }
+  for key in multiple:
+    for i in n:
+      if key == i:
+        multiple[key] = multiple[key] + 1
+
+  if multiple["3"] > multiple["6"] and multiple["3"] > multiple["9"]:
+    return 3
+  elif multiple["6"] > multiple["3"] and multiple["6"] > multiple["9"]:
+    return 6
+  elif multiple["9"] > multiple["6"] and multiple["9"] > multiple["3"]:
+    return 9
+  else:
+    return None
+
+
+
 
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
-  longestRepeated = ''
-  previousCount = 0
-  count = 0
-  for i in range(0, len(s) - 1):
-    if s[i] == s[i + 1]:
-      count = count + 1
+  chars = {}
+  arr = []
+  lastChar = ''
+  for i in s:
+    if i not in chars.keys():
+      # Adding to the dictionary with initial value of 1
+      chars[i] = 1
     else:
-      count = previousCount
-    if count > previousCount:
-      longestRepeated = s[i]
-      previousCount
+      if i == lastChar:
+        chars[i] = chars[i] + 1
 
-  return longestRepeated
+    lastChar = i
+
+
+  maxChar = max(chars, key=chars.get)
+  arr.append(maxChar)
+  for key in chars:
+    if key != maxChar:
+      if chars[key] == chars[maxChar]:
+        arr.append(key)
+      elif chars[key] > chars[maxChar]:
+        arr.append(key)
+        arr.remove(maxChar)
+
+  return arr
+
+
+
+
+
+
+
 
 
 # Part C. is_palindrome
