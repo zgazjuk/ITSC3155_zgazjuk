3b55d41dde9077a1f53720de3f4d6bb428573f38
zgazjuk <zgazjuk@icloud.com>
2022-10-18 16:35:13 -0400

    Flask app #2 - Created a dynamic welcome route

--- flask_app/flask02.py
+++ flask_app/flask02.py
@@ -10,7 +10,8 @@ app = Flask(__name__)     # create an app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    a_user = {'name': 'Zach', 'email': 'zgazjuk@uncc.edu'}
+    return render_template('index.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
