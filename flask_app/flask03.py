f76ef66961c5d334a7a0ff8076eccf2501754507
zgazjuk <zgazjuk@icloud.com>
2022-10-25 16:34:55 -0400

    Flask app #3 - added new note route and view

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -1,6 +1,46 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+import os                 # os is used to get environment variables IP & PORT
+from flask import Flask   # Flask is the web app that we will customize
+from flask import render_template
+
+app = Flask(__name__)     # create an app
+
+# @app.route is a decorator. It gives the function "index" special powers.
+# In this case it makes it so anyone going to "your-url/" makes this function
+# get called. What it returns is what is shown as the web page
+@app.route('/')
+@app.route('/index')
+def index():
+    a_user = {'name': 'Zach', 'email': 'zgazjuk@uncc.edu'}
+    return render_template('index.html', user=a_user)
+
+@app.route('/notes')
+def get_notes():
+    notes = {1:{'title': 'First Note', 'text': 'This is my first note', 'date': '10-1-2020'},
+            2:{'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+            3:{'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+             }
+
+    return render_template('notes.html', notes=notes)
+
+@app.route('/notes/<note_id>')
+def get_note(note_id):
+    notes = {1:{'title': 'First Note', 'text': 'This is my first note', 'date': '10-1-2020'},
+            2:{'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+            3:{'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
+             }
+
+    return render_template('note.html', note=notes[int(note_id)])
+@app.route('/notes/new')
+def new_note():
+    a_user = {'name' : 'Zach', 'email':'zgazjuk@uncc.edu'}
+    return render_template('new.html', user=a_user)
+
+
+app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+
+# To see the web page in your web browser, go to the url,
+#   http://127.0.0.1:5000
+
+# Note that we are running with "debug=True", so if you make changes and save it
+# the server will automatically update. This is great for development but is a
+# security risk for production.
