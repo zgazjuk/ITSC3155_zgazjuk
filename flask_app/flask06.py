f971db6b38ecde2385dcac77ec25a9d24efad41e
zgazjuk <zgazjuk@icloud.com>
2022-11-16 00:05:22 -0500

    adding and displaying comments functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -10,6 +10,8 @@ from forms import RegisterForm
 from flask import session
 import bcrypt
 from forms import LoginForm
+from models import Comment as Comment
+from forms import RegisterForm, LoginForm, CommentForm
 
 
 
@@ -50,11 +52,16 @@ def get_notes():
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     # retrieving user from database
-    a_user = db.session.query(User).filter_by(email='zgazjuk@uncc.edu').one()
-    # retrieving note from database
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
+    if session.get('user'):
 
-    return render_template('note.html', note=my_note, user=a_user)
+        # retrieving note from database
+
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        form = CommentForm()
+
+        return render_template('note.html', note=my_note, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/notes/new', methods=['GET', 'POST'])
@@ -183,6 +190,23 @@ def logout():
         session.clear()
 
     return redirect(url_for('index'))
+
+@app.route('/notes/<note_id>/comment', methods=['POST'])
+def new_comment(note_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        # validate_on_submit only validates using POST
+        if comment_form.validate_on_submit():
+            # get comment data
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(note_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+
+        return redirect(url_for('get_note', note_id=note_id))
+
+    else:
+        return redirect(url_for('login'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
