50a19fe028391653883aecebbed06079583f9a12
zgazjuk <zgazjuk@icloud.com>
2022-11-15 23:28:43 -0500

    flask #6 - login registraion functionality

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, DataRequired, EqualTo, Email
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import Length, Regexp, DataRequired, EqualTo, Email
 from wtforms import ValidationError
 from models import User
 from database import db
@@ -12,7 +12,7 @@ class RegisterForm(FlaskForm):
 
     firstname = StringField('First Name', validators=[Length(1, 10)])
 
-    lastname = StringField('Last Name', validators=[Length(1, 10)])
+    lastname = StringField('Last Name', validators=[Length(1, 20)])
 
     email = StringField('Email', [
         Email(message='Not a valid email address.'),
@@ -26,9 +26,26 @@ class RegisterForm(FlaskForm):
     confirmPassword = PasswordField('Confirm Password', validators=[
         Length(min=6, max=10)
     ])
-
     submit = SubmitField('Submit')
 
     def validate_email(self, field):
         if db.session.query(User).filter_by(email=field.data).count() != 0:
             raise ValidationError('Username already in use.')
+
+
+class LoginForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    email = StringField('Email', [
+        Email(message='Not a valid email address.'),
+        DataRequired()])
+
+    password = PasswordField('Password', [
+        DataRequired(message="Please enter a password.")])
+
+    submit = SubmitField('Submit')
+
+    def validate_email(self, field):
+        if db.session.query(User).filter_by(email=field.data).count() == 0:
+            raise ValidationError('Incorrect username or password.')
