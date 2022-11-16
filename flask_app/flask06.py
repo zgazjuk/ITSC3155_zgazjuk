ad5f4c7a78a995dd54d08edc88a3e59d19e0e2d4
zgazjuk <zgazjuk@icloud.com>
2022-11-15 23:52:24 -0500

    flask #6 - added logout functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -176,7 +176,13 @@ def login():
         # form did not validate or GET request
         return render_template("login.html", form=login_form)
 
+@app.route('/logout')
+def logout():
+    # check if a user is saved in session
+    if session.get('user'):
+        session.clear()
 
+    return redirect(url_for('index'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
